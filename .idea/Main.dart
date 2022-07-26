9315cf22cd6eb2966d7e285dab17c54c850070e9
madahetooo <ieslammedhat@gmail.com>
2022-07-26 21:00:26 +0200

    Adding Dart Details

---
+++ .idea/Main.dart
@@ -0,0 +1,101 @@
+void main() {
+  // var name = "Eslam Medhat"; // String
+  // var age = 26; // Int
+  // print("$name + $age");
+  // int marks = 90; // Int
+  // double pi = 3.14; //double
+  // bool isValid = true; // boolean
+  // var listOfNumbers = [1, 2, 3, 4, 5, 6]; // Array of Int, List of Int
+  // var listOfNames2 = ["Eslam", "Mohamed", "Asmaa", "Omar"]; // Array of String
+  // var listOfNames = {'name': 'Eslam'}; // Mapping, KeyValuePair
+  // var heartSymbol = '\u2665';
+  // var laughSymbol = '\u{1f600}';
+  // print(heartSymbol);
+  // print(laughSymbol);
+
+  // int count =5;
+  // assert(count == 5);
+
+  //Arithmetic OPERATOR
+  // var number1 = 10;
+  // var number2 = 20;
+  // print("Number  1 + Number 2 = ${number1 + number2}");
+  // print("Number  1 - Number 2 = ${number1 - number2}");
+  // print("Number  1 / Number 2 = ${number1 / number2}");
+  // print("Number  1 * Number 2 = ${number1 * number2}");
+
+  //UNARY OPERATOR
+  // var x = 5;
+  // print(x++); // 5
+  // print(x); // 6
+  // print(++x); //7
+  // print(x--); //7
+  // print(x); //6
+
+  //Assignement Operator
+  // var x = 10;
+  // x+=1;
+  // x-=1;
+  // x*=1;
+  // print(x);
+
+  // var x = 30;
+  // var y = 30;
+
+  // var result = x > y;
+  // print(" X is Greater than y : " + result.toString());
+  // //
+  // var result = x < y;
+  // print(" X is Smaller than y : " + result.toString());
+
+  // var result = x >= y;
+  // print(" X is Greater than or equal to  y : " + result.toString());
+
+  // var result = x <= y;
+  // print(" X is Smaller than or equal to y : " + result.toString());
+
+  // var result = x != y;
+  // print(" X is  not equal to y : " + result.toString());
+
+  // var result = x == y;
+  // print(" X is   equal to y : " + result.toString());
+
+  // //Data Type Test
+  // var number = 10;
+  // var fullName = 101;
+  // print(number is int);
+  // print(fullName is String);
+
+  //Logical Operator
+
+  // bool booleanValue1 = true;
+  // bool booleanValue2 = false;
+  //
+  // var result = booleanValue1 && booleanValue2;
+  // print(result);
+
+  // var x = 5;
+  // var y = 4;
+  // var z = 7;
+  //
+  // if (x > y && x > z) {
+  //   print("X is the greatest number ");
+  // }
+  // else{
+  //   print("x is not the greatest number");
+  // }
+
+
+  // var x = 5;
+  // var result = x >10 ? "X is greater than 10"  : " x is lower than 10" ;
+  // print(result);
+
+  // var listOfNames = ["Eslam","Omar","Mahmoud","Ahmed"];
+  // print(listOfNames);
+  // listOfNames.forEach((item) {
+  //   print("${listOfNames.indexOf(item)} : $item");
+  // });
+
+  
+
+}
